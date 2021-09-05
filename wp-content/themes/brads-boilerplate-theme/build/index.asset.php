cc5beadd319aaa22fb4ed6b406ecddf5da134c6d
Annie Kyles <anniek@federation.net.nz>
2021-09-06 11:05:01 +1200

    Installed Brad's Boilerplate Block plugin.

--- wp-content/themes/brads-boilerplate-theme/build/index.asset.php
+++ wp-content/themes/brads-boilerplate-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'a6f8bc415dc2f3f4228749c9c1b6a13e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'bde7f79d29afc7668d6cfacca1a9985b');
